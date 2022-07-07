37833daab8c1c72d2d4b3cc14ab78c48c364cc99
Srinandhan20 <107259556+Srinandhan20@users.noreply.github.com>
2022-07-07 11:04:38 +0530

    Add files via upload

---
+++ slicing1.py
@@ -0,0 +1,18 @@
+# x='department'
+# print(x[2]+x[3]+x[4]+x[5])
+# print(x[0]+x[1]+x[2]+x[3]+x[4]+x[5])
+# a='python'
+# print(a[0:])
+# print(a[:])
+# print(a[1:])
+# print(a[4:])
+# print(a[:2])
+# print(a[4:2])
+
+a='python'
+print(a[-1])
+print(a[:-1])
+print(a[:-2])
+print(a[:-3])
+print(a[:-8])
+
